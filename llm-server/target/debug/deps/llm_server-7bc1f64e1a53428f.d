dd0b84e8687d98c3a04623380c86e1baa9e1c8cc
kobbyidun <plasmadeveloper@outlook.com>
2023-07-08 12:44:44 -0400

    setting up LLM server

---
+++ llm-server/target/debug/deps/llm_server-7bc1f64e1a53428f.d
@@ -0,0 +1,5 @@
+C:\Users\kobbyidun\OneDrive\Comp Sci\Code\Current Projects\PHINEAS-LLM\PHINEAS-LLM\llm-server\target\debug\deps\llm_server-7bc1f64e1a53428f.rmeta: src\main.rs
+
+C:\Users\kobbyidun\OneDrive\Comp Sci\Code\Current Projects\PHINEAS-LLM\PHINEAS-LLM\llm-server\target\debug\deps\llm_server-7bc1f64e1a53428f.d: src\main.rs
+
+src\main.rs:
---
+++ llm-server/target/debug/deps/llm_server-c628aada259e9553.d
@@ -0,0 +1,5 @@
+C:\Users\kobbyidun\OneDrive\Comp Sci\Code\Current Projects\PHINEAS-LLM\PHINEAS-LLM\llm-server\target\debug\deps\llm_server-c628aada259e9553.rmeta: src\main.rs
+
+C:\Users\kobbyidun\OneDrive\Comp Sci\Code\Current Projects\PHINEAS-LLM\PHINEAS-LLM\llm-server\target\debug\deps\llm_server-c628aada259e9553.d: src\main.rs
+
+src\main.rs:
