17387c92d9886f12b13ef4b82552432c1d653132
kobbyidun <plasmadeveloper@outlook.com>
2023-07-08 20:56:44 -0400

    removed program execution bug

--- llm-server/src/main.rs
+++ llm-server/src/main.rs
@@ -5,9 +5,15 @@ use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    
-    let request = String::from_utf8_lossy(&buffer[..]);
+    let request = match stream.read(&mut buffer) {
+        Ok(bytes_read) => {
+            String::from_utf8_lossy(&buffer[..bytes_read]).to_string()
+        }
+        Err(err) => {
+            eprintln!("Failed to read stream: {}", err);
+            return;
+        }
+    };
     
     // Extract the value of the "prompt" query parameter
     let prompt = if let Some(index) = request.find("prompt=") {
@@ -19,22 +25,36 @@ fn handle_client(mut stream: TcpStream) {
     };
     
     // Build the command to execute
-    let cmd = format!("llm llama infer -m model/ggml-wizardLM-7B.q4_2.bin -p \"{}\"", prompt);
+    let cmd = format!("llm llama infer -m C://Users//kobbyidun//OneDrive//CompSci//Code//Projects//PHINEAS-LLM//PHINEAS-LLM//llm-server//src//model//ggml-wizardLM-7B.q4_2.bin -p \"{}\"", prompt);
+    // Print the command
+    println!("{}", cmd);
     
     // Execute the command and capture the output
-    let output: Output = Command::new("sh")
+    let output: Output = match Command::new("cmd")
         .arg("-c")
-        .arg(cmd)
+        .arg(&cmd)
         .output()
-        .expect("Failed to execute command");
+    {
+        Ok(output) => output,
+        //print output.stdout
+        Err(err) => {
+            eprintln!("Failed to execute command: {}", err);
+            return;
+        } 
+    };
     
     // Extract the output as a UTF-8 string
     let response = String::from_utf8_lossy(&output.stdout);
     
     // Send the response back to the client
     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", response);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if let Err(err) = stream.write(response.as_bytes()) {
+        eprintln!("Failed to write response: {}", err);
+    }
+    
+    if let Err(err) = stream.flush() {
+        eprintln!("Failed to flush stream: {}", err);
+    }
 }
 
 fn main() {
@@ -42,9 +62,15 @@ fn main() {
     println!("Server listening on 127.0.0.1:8080");
     
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        thread::spawn(|| {
-            handle_client(stream);
-        });
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(err) => {
+                eprintln!("Failed to establish connection: {}", err);
+            }
+        }
     }
 }
